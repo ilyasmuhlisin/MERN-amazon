994a41655bfc55bb209f9f6ac25fe2225e1dfe40
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2022-11-15 19:14:08 +0700

    list product on home

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import data from "./data";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/product/:slug" element={<ProductScreen />}></Route>
+            <Route path="/" element={<HomeScreen />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
