b6c5af58f7d200d659c1a7ebe0eed44ca5fb6617
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2022-11-16 20:11:38 +0700

    product component and card bootstrap

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useReducer, useState } from "react";
 // import data from "../data";
 import axios from "axios";
-import { Link } from "react-router-dom";
+
 // untuk mencatat status
 import logger from "use-reducer-logger";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../components/Product";
 
 // parameter 1 current state and paramter 2 action
 const reducer = (state, action) => {
@@ -53,22 +56,13 @@ function HomeScreen() {
         ) : error ? (
           <div>{error}</div>
         ) : (
-          products.map((product) => (
-            <div className="product" key={product.slug}>
-              <Link to={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
         )}
       </div>
     </div>
