caf5c6525da58a9d52d370fb596f88e8dd427a7f
ilyasmuhlisin <21ilyasmhlsn@gmail.com>
2022-11-18 18:36:40 +0700

    complete add to chart update

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -1,5 +1,6 @@
 import { createContext, useReducer } from "react";
 
+// store wadah untuk menyimpan value/state secara global
 export const Store = createContext();
 
 const initialState = {
@@ -7,23 +8,36 @@ const initialState = {
     cartItems: [],
   },
 };
+// fungsi dimana store value/state sehingga reducer sebagai update distore
 function reducer(state, action) {
   switch (action.type) {
     case "CART_ADD_ITEM":
+      // add to chart without double count
+      const newItem = action.payload;
+      const existItem = state.cart.cartItems.find(
+        (item) => item._id === newItem._id
+      );
+      const cartItems = existItem
+        ? state.cart.cartItems.map((item) =>
+            item._id === existItem._id ? newItem : item
+          )
+        : [...state.cart.cartItems, newItem];
+      return { ...state, cart: { ...state.cart, cartItems } };
+
       // add to cart
-      return {
-        ...state,
-        cart: {
-          ...state.cart,
-          cartItems: [...state.cart.cartItems, action.payload],
-        },
-      };
+      // return {
+      //   ...state,
+      //   cart: {
+      //     ...state.cart,
+      //     cartItems: [...state.cart.cartItems, action.payload],
+      //   },
+      // };
     default:
       return state;
   }
 }
-
 export function StoreProvider(props) {
+  // dispatch adalah proses pengambilan instruksi reducer
   const [state, dispatch] = useReducer(reducer, initialState);
   const value = { state, dispatch };
   return <Store.Provider value={value}>{props.children} </Store.Provider>;
